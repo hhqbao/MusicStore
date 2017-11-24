cbda1da1836319170210792afb7d08285c1750bb
Jason Ho <Jason Ho@DESKTOP-S86R0PJ>
2017-11-25 01:56:11 +1100

    Implemented encoding and decoding string

---
+++ MusicStore.Services/MyLibrary.cs
@@ -0,0 +1,19 @@
+﻿namespace MusicStore.Services
+{
+    public class MyLibrary
+    {
+        public static string EncodeString(string inputString)
+        {
+            byte[] toEncodeAsBytes = System.Text.Encoding.UTF8.GetBytes(inputString);
+
+            return System.Convert.ToBase64String(toEncodeAsBytes);
+        }
+
+        public static string DecodeString(string inputString)
+        {
+            byte[] encodedDataAsBytes = System.Convert.FromBase64String(inputString);
+
+            return System.Text.Encoding.UTF8.GetString(encodedDataAsBytes);
+        }
+    }
+}
